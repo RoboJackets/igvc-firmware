ab99a47e8b684fc965d3a495ba367b7874f16202
dallas-d <dallas.downing@comcast.net>
2020-04-05 15:35:50 -0400

    Capitals and constexpr

--- src/mbed/constants.h
+++ src/mbed/constants.h
@@ -9,22 +9,20 @@
  * Do NOT use macros; instead define a constant here
  */
 
-const bool g_debug = false;
+constexpr bool G_DEBUG = false;
 
 /* ethernet setup variables */
-const int g_server_port = 5333;
-const int g_buffer_size = 256;
-const int g_max_messages = 1; // backlog of messages the server should hold
-const int g_timeout_ms = 50;  // timeout for blocking read operations
-constexpr const char* g_mbed_ip = "192.168.1.20";
-constexpr const char* g_netmask = "255.255.255.0";
-constexpr const char* g_computer_ip = "192.168.1.21";
+constexpr int G_SERVER_PORT = 5333;
+constexpr int G_BUFFER_SIZE = 256;
+constexpr const char* G_MBED_IP = "192.168.1.20";
+constexpr const char* G_NETMASK = "255.255.255.0";
+constexpr const char* G_COMPUTER_IP = "192.168.1.21";
 
 
 /* calculation constants */
-const double g_wheel_circum = 1.092;
-const double g_gear_ratio = 32.0;
-const int g_ticks_per_rev = 48;
-const double g_meters_per_tick = g_wheel_circum / (g_ticks_per_rev * g_gear_ratio);
+constexpr double G_WHEEL_CIRCUM = 1.092;
+constexpr double G_GEAR_RATIO = 32.0;
+constexpr int G_TICKS_PER_REV = 48;
+constexpr double G_METERS_PER_TICK = G_WHEEL_CIRCUM / (G_TICKS_PER_REV * G_GEAR_RATIO);
 
 #endif //FIRMWARE_UTIL
