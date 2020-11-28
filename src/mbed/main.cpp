8ce5e014544445623ec005a08c2a0f54dabdff1e
matthewhannay567 <matthew.scott.hannay@gmail.com>
2020-11-28 13:01:14 -0500

    Properly encoded debug string message

--- src/mbed/main.cpp
+++ src/mbed/main.cpp
@@ -270,6 +270,13 @@ int main()
   }
 }
 
+bool encode_string(pb_ostream_t *stream, const pb_field_t *field, void * const *arg){
+    const char* str = static_cast<const char*>(*arg);
+    if (!pb_encode_tag_for_field(stream, field))
+        return false;
+    return pb_encode_string(stream, (uint8_t*)str, strlen(str));
+}
+
 bool sendResponse(TCPSocket &client)
 {
   /* protocol buffer to hold response message */
@@ -323,7 +330,9 @@ bool sendResponse(TCPSocket &client)
 
   response.desired_speed_l = g_desired_speed_l;
   response.desired_speed_r = g_desired_speed_r;
-  response.debug = "";
+
+  response.debug.arg = (void*)""; // set debug message here
+  response.debug.funcs.encode = &encode_string;
 
   /* encode the message */
   ostatus = pb_encode(&ostream, ResponseMessage_fields, &response);
