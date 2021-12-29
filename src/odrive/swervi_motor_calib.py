47f577b52f088abaf2ff4fccb2e93a56a6816529
AR2100 <aroach34@gatech.edu>
2021-12-29 17:09:16 -0500

    imporved odrive configuration scripts, not tested on swervi

---
+++ src/odrive/swervi_motor_calib.py
@@ -0,0 +1,4 @@
+from tools.swervi_tools import orange_drive_calib, orange_steering_calib, orange_full_calib
+from tools.swervi_tools import red_drive_calib, red_steering_calib, red_full_calib
+from tools.swervi_tools import white_drive_calib, white_steering_calib, white_full_calib
+from tools.swervi_tools import brown_drive_calib, brown_steering_calib, brown_full_calib
